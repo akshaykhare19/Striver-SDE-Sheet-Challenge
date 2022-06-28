3964d3f7c70454fd9bde59b435a2540a484c6351
Akshay Khare <akhare931@gmail.com>
2022-06-29 01:57:17 +0530

    Set matrix zeroes

--- Arrays/SetMatrixZeroes.java
+++ Arrays/SetMatrixZeroes.java
@@ -1,5 +1,3 @@
-package Arrays;
-
 /*
 Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
 
@@ -99,16 +97,69 @@ public class SetMatrixZeroes
         }
     }
 
-    public static void main(String args[]) {
-        int arr[][] = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
-        bruteSetZeroes(arr);
-        System.out.println("The Final Matrix is ");
+
+    //Some problem with optimal set zeroes function right now
+    // //TC - O(n*m)
+    // //SC - O(1)
+    // public static void optimalSetZeroes(int[][] matrix) {
+    //     int m = matrix.length;
+    //     int n = matrix[0].length;
+    //     int col0 = 1;
+        
+    //     for(int i = 0; i < m; i++){
+    //         if(matrix[i][0]==0) col0 = 0;
+    //         for(int j = 1; j < n; j++){
+    //             if(matrix[i][j]==0){
+    //                 matrix[i][0] = 0;
+    //                 matrix[0][j] = 0;
+    //             }
+    //         }
+    //     }
+        
+    //     for(int i = m-1; i >= 0; i--){
+    //         for(int j = n-1; j >= 1; j--){
+    //             if(matrix[i][0]==0 || matrix[0][j]==0){
+    //                 matrix[i][j] = 0;
+    //             }
+    //             if(col0 == 0) matrix[i][0] = 0;
+    //         }
+    //     }
+    // }
+
+    public static void display(int arr[][])
+    {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Question Set Zeroes ");
+        int arr1[][] = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        display(arr1);
+        
+        System.out.println("Brute Set Zeroes ");
+        bruteSetZeroes(arr1);
+        display(arr1);
+
+        System.out.println("Question Set Zeroes ");
+        int arr2[][] = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        display(arr2);
+        
+        System.out.println("Better Set Zeroes ");
+        betterSetZeroes(arr2);
+        display(arr2);
+
+        // System.out.println("Question Set Zeroes ");
+        // int arr3[][] = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        // display(arr3);
+        
+        // System.out.println("Optimal Set Zeroes ");
+        // optimalSetZeroes(arr3);
+        // display(arr3);
     }
 }
 
