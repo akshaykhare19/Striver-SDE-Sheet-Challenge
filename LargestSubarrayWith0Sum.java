d22ed73ccf4df525d10c88359d31edb3f7c60ad6
Akshay Khare <akhare931@gmail.com>
2022-07-08 23:13:32 +0530

    Added description

--- LargestSubarrayWith0Sum.java
+++ LargestSubarrayWith0Sum.java
@@ -1,3 +1,32 @@
+/*
+ * 
+Largest subarray with 0 sum
+Easy Accuracy: 46.94% Submissions: 100k+ Points: 2
+Lamp
+This problem is part of GFG SDE Sheet. Click here to view more.  
+
+Given an array having both positive and negative integers. The task is to compute the length of the largest subarray with sum 0.
+
+Example 1:
+
+Input:
+N = 8
+A[] = {15,-2,2,-8,1,7,10,23}
+Output: 5
+Explanation: The largest subarray with
+sum 0 will be -2 2 -8 1 7.
+
+Your Task:
+You just have to complete the function maxLen() which takes two arguments an array A and n, where n is the size of the array A and returns the length of the largest subarray with 0 sum.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 <= N <= 105
+-1000 <= A[i] <= 1000, for each valid i
+ */
+
 import java.util.*;
 public class LargestSubarrayWith0Sum
 {
