17c912d39530847a83375ebb7111d3d8ba16efac
Akshay Khare <akhare931@gmail.com>
2022-07-15 23:19:50 +0530

    Nth root of M

---
+++ NthRootOfM.java
@@ -0,0 +1,11 @@
+public class NthRootOfM
+{
+    public static void main(String args[])
+    {
+        int n = 3;
+        long m = 27;
+
+        //Approach 1
+        System.out.println( Math.pow(m, 1/(double)n));
+    }    
+}
